c5103b9c34a8f08e7fde260d2fcdddde39c996fe
Alejo Becerra <alejo.mann@gmail.com>
2022-03-22 08:31:47 -0300

    TP - Clase 9

---
+++ ItemDetail.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ItemCount from "./ItemCount.js"
+import { useState } from 'react'
+
+const ItemDetail = ({item}) => {
+
+    let [estado,setEstado] = useState(0)
+
+    //agrego al carrito
+    const onAdd = (cantidadSeleccionada) => {
+        console.log("Agregar al carrito: " + cantidadSeleccionada + " " + item.title);
+    }
+
+    return (
+        <div className='contDetalle'>
+            <div className='contFoto'>
+                <img src={item.image} alt="" className="card-img-top w-100" />
+            </div>
+            <div className="contData">
+                <span>{item.category}</span>
+                <h3>{item.title}</h3>
+                <p className='price'>{"$"}{item.price}</p>
+                <p className="descripcion">{item.description}</p>
+                <ItemCount stock={10} onAdd={onAdd} initial={1}/>
+            </div>
+        </div>
+    )
+}
+
+export default ItemDetail
